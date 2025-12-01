3aa05b1ad835b3042deae708a404dae27d6a65fc
Zantgo <100251517+zantgo@users.noreply.github.com>
2025-12-01 13:49:51 -0300

    Update __init__.py

--- core/bot_controller/__init__.py
+++ core/bot_controller/__init__.py
@@ -11,15 +11,8 @@ BotController.
 """
 
 # --- Exposición de la API Pública ---
-# Se importa el módulo `_api` y se le da un alias público y corto `api`.
-# Esto permite a otros módulos acceder a las funciones del BotController
-# de forma limpia y desacoplada, por ejemplo: `from core.bot_controller import api`.
 from . import _api as api
 
-# --- Control de lo que se exporta con 'from core.bot_controller import *' ---
-# Es una buena práctica definir `__all__` para declarar explícitamente la
-# interfaz pública del paquete. En este caso, solo queremos exponer el
-# módulo `api`.
 __all__ = [
     'api',
 ]
